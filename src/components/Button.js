21e2fe9824cbd7f22dfbe2051393b1bd3619b850
Luis Porras <lporras16@gmail.com>
2018-12-10 15:31:20 -0300

    destructuring language in button

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,7 +9,7 @@ class Button extends React.Component {
                 {(color) =>
                     <button className={`ui button ${color}`}>
                         <LanguageContext.Consumer>
-                            {(value) => value.language === 'english' ? 'Submit' : 'Voorleggen'}
+                            {({ language }) => language === 'english' ? 'Submit' : 'Voorleggen'}
                         </LanguageContext.Consumer>
                     </button>
                 }
