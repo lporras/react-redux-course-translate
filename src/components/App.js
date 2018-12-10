b7b62f942a8c777ec8ddcec502864e3dddb2dd5e
Luis Porras <lporras16@gmail.com>
2018-12-10 14:14:15 -0300

    adding components Field, Button and UserCreate

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component {
     state = { language: 'english' }
@@ -13,7 +14,7 @@ class App extends React.Component {
                     <i className="flag us" onClick={() => this.onLanguageChange('english')}></i>
                     <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}></i>
                 </div>
-                {this.state.language}
+                <UserCreate />
             </div>
         );
     }
